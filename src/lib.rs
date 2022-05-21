5c82621d5d502072e89a11175668c35060aad1e1
Radu Cazacu <cazacuraduic@gmail.com>
2022-05-21 20:59:57 +0300

    Init:

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+mod random;
+mod minesweeper;
+
+use minesweeper::*;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+	fn alert(s: &str);
+}
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+	alert(&format!("Hello, {}", name))
+}
